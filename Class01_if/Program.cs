b8ed5a02ccbef164829ed6d2a90ac45cd079adb3
mguadarrama <manuel.jonathan@gmail.com>
2013-07-25 16:28:25 +0200

    Actualizado clase 2

---
+++ Class01_if/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Class01_if
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Probando el if");
+            Console.WriteLine("Hola,");
+
+            string resultado = Console.ReadLine();
+
+
+            if (resultado == "Hola")
+            {
+                Console.WriteLine("Como Estas?");
+            }
+            else if (resultado == "Adios")
+            {
+                Console.WriteLine("Hasta Luego");
+            }
+            else
+            {
+                Console.WriteLine("Saluda primero, no?");
+            }
+
+
+            Console.WriteLine("Probando el switch");
+            resultado = Console.ReadLine();
+
+            switch (resultado)
+            {
+                case "Hola":
+                    Console.WriteLine("Como Estas?");
+                    break;
+                case "Adios":
+                    Console.WriteLine("Hasta Luego");
+                    break;
+                default:
+                    Console.WriteLine("Saluda primero, no?");
+                    break;
+            }
+
+        }
+    }
+}
---
+++ Class02_loop/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class02_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Probando loops");
+            
+            int e = 0;
+            while (e <= 5)
+            {
+                Console.WriteLine("e = " + e);
+                //e = e + 1;
+                e++;
+            }
+
+
+            for (int i = 0; i <= 5; i++)
+            {
+                Console.WriteLine("i = {0}", i);
+            }
+
+
+
+            int[] numeros = { 1, 2, 3, 4, 5 };
+            foreach (int numero in numeros)
+            {
+                Console.WriteLine("Que numero " +
+                                  "estoy " +
+                                  "leyendo " +
+                                  "ahora?????: " +
+                                  "{0}", numero);
+            }
+
+
+
+        }
+    }
+}
