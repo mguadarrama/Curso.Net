cad1b40f1990ebc4a729dfb2aff4b8cb159963c9
mguadarrama <manuel.jonathan@gmail.com>
2013-08-09 00:18:21 +0200

    add nugets

---
+++ Class15_Todo/Controllers/ManuelController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Class15_Todo.Controllers
+{
+    public class ManuelController : Controller
+    {
+        //
+        // GET: /Manuel/
+
+        public string Index()
+        {
+            return "Hola";
+        }
+
+    }
+}
