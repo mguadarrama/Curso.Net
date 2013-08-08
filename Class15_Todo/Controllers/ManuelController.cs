584355489bca9b8bbb813f856bd6b985b92c3710
mguadarrama <manuel.jonathan@gmail.com>
2013-08-09 01:32:00 +0200

    news in mvc

--- Class15_Todo/Controllers/ManuelController.cs
+++ Class15_Todo/Controllers/ManuelController.cs
@@ -11,9 +11,35 @@ namespace Class15_Todo.Controllers
         //
         // GET: /Manuel/
 
+
+        // Controllers within the MVC pattern are responsible for responding to user input, often making
+        // changes to the model in response to user input. In this way, controllers in the MVC pattern are concerned
+        // with the fl ow of the application, working with data coming in, and providing data going out
+        // to the relevant view.
+
+
+
+
         public string Index()
         {
             return "Hola";
+        }
+
+        public string Detalles(int id)
+        {
+            return "He recibido el numero:" + id;
+        }
+
+
+
+
+
+
+        public ViewResult Index2()
+        {
+            ViewBag.UnParametro= "Pasando Params";
+            return View();
+            //return View("View1");
         }
 
     }
